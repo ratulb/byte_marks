5e7a00423088ed03f1c6ecfb3684ef1af1c54e0c
Ratul <ratul75@hotmail.com>
2022-01-20 02:12:48 +0530

    Update marked.rs

--- src/marked.rs
+++ src/marked.rs
@@ -283,15 +283,4 @@ mod test {
 }
 
 
-///A convinient macros
-#[macro_export]
-macro_rules! function {
-    () => {{
-        fn f() {}
-        fn type_name_of<T>(_: T) -> &'static str {
-            std::any::type_name::<T>()
-        }
-        let name = type_name_of(f);
-        &name[..name.len() - 3]
-    }};
-}
+
